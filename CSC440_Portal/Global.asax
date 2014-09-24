2ec38dacf611992e433561acc50a7588c2b84b95
Adam Howard <awhoward112@gmail.com>
2014-09-24 14:32:09 -0400

    initial commit

---
+++ CSC440_Portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSC440_Portal.MvcApplication" Language="C#" %>
---
+++ CSC440_Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSC440_Portal.Startup))]
+namespace CSC440_Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

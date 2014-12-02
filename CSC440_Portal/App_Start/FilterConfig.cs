c311ac881ad59a44475e97d8c760edbac1b5bf1e
Adam Howard <awhoward112@gmail.com>
2014-12-01 20:29:48 -0500

    added global filter to require user authentication on non-login pages

--- CSC440_Portal/App_Start/FilterConfig.cs
+++ CSC440_Portal/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace CSC440_Portal
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }

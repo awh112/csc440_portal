e008f32f22a811cb48eb20fd84024353844912a2
Adam Howard <awhoward112@gmail.com>
2014-12-01 20:45:54 -0500

    added unit test project to solution and changed class diagram

---
+++ CSC440_Portal.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSC440_Portal.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
